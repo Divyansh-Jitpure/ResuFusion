2a835bce307b8e72d476fed4e8fc10f26ee5294f
Divyansh Jitpure <divyanshjit2022@gmail.com>
2025-02-12 17:14:41 +0530

    Update

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -5,7 +5,9 @@ const Home = () => {
   return (
     <div className="flex justify-center">
       <div className="*:my-30">
-        <div className="h-72 w-[800px] bg-red-50"></div>
+        <div className="h-72 w-[800px] bg-red-50">
+          <p className="text-center text-5xl font-bold">Placeholder</p>
+        </div>
       </div>
     </div>
   );
