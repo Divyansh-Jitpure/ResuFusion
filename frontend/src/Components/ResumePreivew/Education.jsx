35bb51bdd47066f8788ce30b9f423505fec3806b
Divyansh Jitpure <divyanshjit2022@gmail.com>
2025-03-11 22:34:30 +0530

    Resume Preview Skills Section Done

--- frontend/src/Components/ResumePreivew/Education.jsx
+++ frontend/src/Components/ResumePreivew/Education.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Education = ({ education }) => {
-  return <div>Education</div>;
+  return (
+    <div className="text-center">
+      <h2 className="mx-5 border-b pb-1 text-xl font-medium text-gray-700">
+        Education
+      </h2>
+    </div>
+  );
 };
 
 export default Education;
--- frontend/src/Components/ResumePreivew/Skills.jsx
+++ frontend/src/Components/ResumePreivew/Skills.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Skills = ({ skills }) => {
-  return <div>Skills</div>;
+  return (
+    <div className="text-center">
+      <h2 className="mx-5 border-b pb-1 text-xl font-medium text-gray-700">
+        Skills
+      </h2>
+      <section className="flex flex-wrap justify-center py-3">
+        {skills.map((skill, index) => {
+          return (
+            <span key={index} className="text-gray-600">
+              {index > 0 && <span className="mx-2">•</span>}
+              {skill}
+            </span>
+          );
+        })}
+      </section>
+    </div>
+  );
 };
 
 export default Skills;
