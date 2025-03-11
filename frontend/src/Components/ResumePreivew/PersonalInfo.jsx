a15d25c7beec2d26f22b6f7521d05e182edb5243
Divyansh Jitpure <divyanshjit2022@gmail.com>
2025-03-11 20:04:49 +0530

    Resume Preview PersonalInfo Section Done

--- frontend/src/Components/ResumePreivew/PersonalInfo.jsx
+++ frontend/src/Components/ResumePreivew/PersonalInfo.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const PersonalInfo = ({ personalInfo }) => {
-  return <div>PersonalInfo</div>;
+  return (
+    <div className="mx-5 flex flex-col items-center border-b py-2">
+      <h1 className="text-6xl">{personalInfo.fullName}</h1>
+      {/* Add this in the resume Form Later \/ */}
+      <h2 className="text-xl font-medium text-gray-700">System Engineer</h2>
+      <section className="flex gap-2 text-gray-500">
+        <div className="">{personalInfo.email}</div> |
+        <div className="">{personalInfo.phone}</div> |
+        <div className="">{personalInfo.address}</div>
+      </section>
+    </div>
+  );
 };
 
 export default PersonalInfo;
