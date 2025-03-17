f43c1cb65aecf1678a3f8a6b9a9358658792837f
Divyansh Jitpure <divyanshjit2022@gmail.com>
2025-03-17 23:28:31 +0530

    Download works

--- frontend/src/Components/ResumePreivew/ResumePreview.jsx
+++ frontend/src/Components/ResumePreivew/ResumePreview.jsx
@@ -1,13 +1,25 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 import API from "../../utils/api";
 import { AuthContext } from "../../context/AuthContext";
 import { useParams } from "react-router";
 import Basic from "../../Pages/Templates/TemplateDesigns/Basic/Basic";
+import { useReactToPrint } from "react-to-print";
 
 const ResumePreview = () => {
   const { resumeId: resumeId } = useParams();
   const { user } = useContext(AuthContext);
   const [resumeData, setResumeData] = useState(null);
+
+  const contentRef = useRef(null);
+
+  const reactToPrintContent = () => {
+    return contentRef.current;
+  };
+
+  const handlePrint = useReactToPrint({
+    content: () => contentRef.current,
+    documentTitle: "YourResume",
+  });
 
   const getResumeData = async () => {
     try {
@@ -26,13 +38,23 @@ const ResumePreview = () => {
 
   return (
     <div className="flex flex-col items-center">
-      <div className="mt-22 flex min-h-screen flex-col items-center justify-center">
+      <div className="my-10 mt-22 flex min-h-screen flex-col items-center justify-center gap-5">
         <h2 className="mb-2 cursor-default text-4xl font-semibold text-gray-50">
           Resume Preview
         </h2>
+        <button
+          className="mx-1 flex items-center gap-1 rounded-2xl bg-[#D84040] px-3 py-1 text-[17px] font-medium transition-all hover:cursor-pointer hover:bg-[#ff2d2d]"
+          onClick={() => handlePrint(reactToPrintContent)}
+          type="button"
+        >
+          Download
+        </button>
         {resumeData && (
           // Resume Container
-          <div className="mb-10 flex h-[1123px] w-[794px] flex-col gap-3 overflow-auto bg-white">
+          <div
+            ref={contentRef}
+            className="flex h-[1123px] w-[794px] flex-col gap-3 bg-white"
+          >
             {resumeData.template === "basic" && (
               <Basic resumeData={resumeData} />
             )}
