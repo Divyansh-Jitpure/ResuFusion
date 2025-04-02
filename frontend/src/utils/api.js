21903714ef435ef1af8c5a453380a2d6194c6c94
Divyansh Jitpure <divyanshjit2022@gmail.com>
2025-04-03 00:15:51 +0530

    update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,11 +1,8 @@
 import axios from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 // Setting up Axios instance with base configuration
 const API = axios.create({
-  baseURL: process.env.API_BASE_URL, // Backend API URL
+  baseURL: import.meta.env.API_BASE_URL, // Backend API URL
   headers: { "Content-Type": "application/json" }, // Default headers
   withCredentials: true, // Ensures cookies are sent with requests
 });
