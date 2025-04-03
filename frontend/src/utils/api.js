51bb4e353eb4c8c37b8ab48c480194df0d49dede
Divyansh Jitpure <divyanshjit2022@gmail.com>
2025-04-03 10:23:07 +0530

    update

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Setting up Axios instance with base configuration
 const API = axios.create({
-  baseURL: import.meta.env.API_BASE_URL, // Backend API URL
+  baseURL: "https://resufusion.onrender.com", // Backend API URL
   headers: { "Content-Type": "application/json" }, // Default headers
   withCredentials: true, // Ensures cookies are sent with requests
 });
